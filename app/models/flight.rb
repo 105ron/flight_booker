1a0fd6a8b30306f968e3b59f9741be74f8540be4
rhys <rbrooker105@gmail.com>
2016-10-19 19:50:10 +1100

    add flights controller, set root to flights#index

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
 	belongs_to :from_airport, :class_name => "Airport",    :foreign_key => :origin_id
   belongs_to :to_airport,  :class_name => "Airport",    :foreign_key => :destination_id
-
+  validates :origin_id, presence: true
+  validates :destination_id, presence: true
 
 end
