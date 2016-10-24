f1b2c91ebc79dd459dbfac3bcc596830bd33ec9e
rhys <rbrooker105@gmail.com>
2016-10-24 16:02:20 +1100

    add passengers controller and display users show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'flights#index'
   get '/bookings', to: 'bookings#new'
   resources :flights, only: [:index, :show]
-  resources :bookings, only: [:new, :create, :show]
+  resources :bookings, only: [:new, :create, :show] 
+  resources :passengers, only: [:show]
 end
